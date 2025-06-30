c34f754c1ffde7d0723c4e958edc82bb9146031e
Zeus <zeus@admin-6s-iMac.local>
2025-06-30 13:21:43 +0800

    done

---
+++ month01/week03/session07/01_intro.py
@@ -0,0 +1,13 @@
+#inroduction into python
+print(10*7)
+print(8*3.57)
+print(14-14)
+print(1567-789)
+print(10+7)
+print(26/5)
+#order of opertions
+print((5+30)*20)
+
+#i want 700
+
+print((5+30)*20/10)#70.0
---
+++ month01/week03/session07/02_variables.py
@@ -0,0 +1,22 @@
+#num counter= 0
+#variable
+fred = 100
+print("fred")
+print(fred)
+
+fullname=("Bold_Erdene Enhtsetseg")
+
+print(fullname)
+
+
+my_age= 24
+
+print(my_age)
+
+friend_age=26
+
+print(friend_age)
+
+av=(my_age+friend_age)/2
+print (av)
+
