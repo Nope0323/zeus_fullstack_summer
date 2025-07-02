558e0c64b2401a5104a7b2310092b19a7558ebff
Zeus <zeus@admin-6s-iMac.local>
2025-07-02 13:15:40 +0800

    fgfgf

---
+++ month01/week03/session08/03_python_lists.py
@@ -0,0 +1,63 @@
+wizard_list='spider legs, toe of frog, bat wing, slug butter, snake dandruff'
+print(wizard_list)
+print(wizard_list[0:11])
+
+wizard_list=['spider legs', 'toe of frog', 'bat wing', 'slug butter', 'snake dandruff']
+
+print(wizard_list)
+print(wizard_list[0])
+
+print(wizard_list[0])
+print(wizard_list[1])
+print(wizard_list[2])
+print(wizard_list[3])
+print(wizard_list[4])
+
+print(f'Length of the wixard list is {len(wizard_list)}')
+
+wizard_list[2]= 'snail tongue'
+
+print(wizard_list)
+wizard_list[4]='snake poison'
+
+print(wizard_list)
+
+print(wizard_list[2:5])
+
+print(wizard_list[-1])
+
+print(wizard_list[-3])
+
+some_numbers= [1,2,5,10,20]
+
+some_strings = ["which", "Which","is","Which"]
+
+numbwe_and_strings= ["why","Was",6,"afraid", "of", 7,"because",7,8,9]
+print(numbwe_and_strings)
+
+mylist = [some_numbers,some_strings]
+print(mylist)
+
+wizard_list.append('bear burp')
+print(wizard_list)
+mandrake="mandrake"
+hemlock="hemlock"
+gas="gas"
+
+wizard_list.append('mandrake')
+wizard_list.append('hemlock')
+wizard_list.append('gas')
+
+
+print(wizard_list)
+#mandrake ,hemlock,swap. gas
+
+
+
+del wizard_list[4]
+del wizard_list[2]
+
+list1= [10,4,5,-1,0 ,11]
+
+(list1.sort())
+print(list1)
