407c4211e0ec89fa57b51a0fcd661304058e3c03
Nope0323 <xprotbo457@gmail.com>
2025-07-28 08:06:23 +0800

    shape

--- month02/week05/session14/exersice/04_exersice.py
+++ month02/week05/session14/exersice/04_exersice.py
@@ -13,10 +13,27 @@ def square(x, y,size , color):
     turtle.end_fill()
 square(-500,400,800, 'black')
 square(-500,400,400,'red')
-square(-45,400,300,'yellow')
-square(-500,400,50,'blue')
-square(-500,-40,50,'blue')
-square(-500,-60,50,'blue')
+square(-50,400,310,'yellow')
+square(-50,60,150,'blue')
+square(80,60,150,'blue')
+square(150,60,150,'blue')
+square(-50,-135,265,'red')
+square(35,-135,265,'red')
+
+square(-500,-30,60,'blue')
+square(-500,-60,60,'blue')
+square(-500,-90,60,'blue')
+square(-500,-120,60,'blue')
+square(-500,-160,60,'blue')
+square(-500,-190,60,'blue')
+square(-500,-220,60,'blue')
+square(-500,-240,60,'blue')
+square(-500,-260,60,'blue')
+square(-500,-290,60,'blue')
+square(-500,-310,60,'blue')
+
+square(-420,-30,340,'yellow')
+
 
 
 
