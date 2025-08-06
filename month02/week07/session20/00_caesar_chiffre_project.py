54cc6fc5f1dec400f47cdb0c430a7850a067b40c
Zeus <zeus@admin-6s-iMac.local>
2025-08-06 11:05:43 +0800

    shiish

---
+++ month02/week07/session20/00_caesar_chiffre_project.py
@@ -0,0 +1,14 @@
+Menu="""
+1."encrypt" (шифрлэх)
+2."decrypt" (тайлах)
+
+Your selection:
+"""
+
+welcome_message = "Цезарийн шифр програмд тавтай морил!"
+print(welcome_message)
+
+code= input('code:')
+code = int(code)
+
+def caesar_cipher:
