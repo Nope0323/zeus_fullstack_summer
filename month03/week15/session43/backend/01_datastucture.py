304bdd14f926de55cd6aa6ae8a8183377d14c619
Zeus <zeus@admin-6s-iMac.local>
2025-10-01 12:47:30 +0800

    hhh

--- month03/week15/session43/backend/01_datastucture.py
+++ month03/week15/session43/backend/01_datastucture.py
@@ -24,3 +24,40 @@ list_a = [1, 2, 3, 4, 5, "алим"]
 list_b = [4, 5, 6, 7, 8, "алим"]
 commons = find_common_elements(list_a, list_b)
 print(f"Ерөнхий элементүүд: {commons}")
+
+def most_frequent_char(text):
+    """Текст доторх хамгийн олон давтагдсан тэмдэгтийг олно."""
+    counts = {}
+    # TODO
+    for n in text:
+        counts[n] = counts.get(n,0) + 1 # {"п":1, "р":2}
+
+
+    most_frequent = None #k
+    max_count = -1 #v
+    # TODO
+    for k, v in counts.items():
+        if v > max_count:
+            most_frequent = k
+            max_count = v
+    return most_frequent, max_count
+
+    
+my_text = "програмчлалын үндсэн ойлголтууд"
+print(f"Хамгийн олон орсон тэмдэгт: '{most_frequent_char(my_text)}'")
+
+def add_contact(book, name, phone):
+    """Утасны дэвтэрт шинэ контакт нэмнэ."""
+# TODO
+def find_contact(book, name):
+    """Нэрээр нь контакт хайж, олдвол мэдээллийг буцаана."""
+# TODO
+# Утасны дэвтэр (толь бичгийн жагсаалт)
+contact_book = []
+add_contact(contact_book, "Болд", "9911XXXX")
+add_contact(contact_book, "Сарнай", "9988XXXX")
+found = find_contact(contact_book, "Сарнай")
+if found:
+    print(f"Олдсон: {found}")
+else:
+    print("Контакт олдсонгүй.")
