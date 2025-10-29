e39c205c35df01fcfa3ab70f7a4ba714ebe40f2f
Zeus <zeus@admin-6s-iMac.local>
2025-10-29 13:03:45 +0800

    hhih yum uldsen01

--- month03/week19/session56/library_project/catalog/admin.py
+++ month03/week19/session56/library_project/catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from.models import Author, Member,Book
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Author)
+admin.site.register(Author)
