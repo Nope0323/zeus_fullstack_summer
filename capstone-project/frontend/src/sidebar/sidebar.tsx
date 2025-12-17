99a9fc4bbdf82d628557eabbced3c1c3c7b806e3
Zeus <zeus@admin-6s-iMac.local>
2025-12-17 12:53:39 +0800

    side bar

--- capstone-project/frontend/src/sidebar/sidebar.tsx
+++ capstone-project/frontend/src/sidebar/sidebar.tsx
@@ -14,42 +14,45 @@ export default function Sidebar() {
   return (
     <aside className="sidebar-left">
         <div className="menu-items">
-        <div className="sidebar-item">
-          <DashboardIcon />
-          <a href="/dashboard" className="sidebar-link">Dashboard</a>
-        </div>
+          <div className="menu-item">
+            <DashboardIcon />
+            <a href="/dashboard" className="sidebar-link">Dashboard</a>
+          </div>
 
-        <div className="sidebar-item">
-        <InventoryIcon/>
-        <a href="/dashboard" className="sidebar-link">Inventory</a>
-        </div>
+          <div className="menu-item">
+            <InventoryIcon />
+            <a href="/dashboard" className="sidebar-link">Inventory</a>
+          </div>
 
-        <div className="sidebar-item">
-        <ReportsIcon/>
-        <a href="/dashboard" className="sidebar-link">Reports</a>
-        </div>
+          <div className="menu-item">
+            <ReportsIcon />
+            <a href="/dashboard" className="sidebar-link">Reports</a>
+          </div>
 
-        <div className="sidebar-item">
-        <UserCircleIcon/>
-        <a href="/dashboard" className="sidebar-link">Suppliers</a>
-        </div>
+          <div className="menu-item">
+            <UserCircleIcon />
+            <a href="/dashboard" className="sidebar-link">Suppliers</a>
+          </div>
 
-        <div className="sidebar-item">
-        <OrdersIcon />
-        <a href="/dashboard" className="sidebar-link">Orders</a>
+          <div className="menu-item">
+          <OrdersIcon />
+          <a href="/dashboard" className="sidebar-link">Orders</a>
+          </div>
+
+          <div className="menu-item">
+            <ManageStoreIcon />
+            <a href="/dashboard" className="sidebar-link">Manage Store</a>
+          </div>
         </div>
-        <div className="sidebar-item">
-        <ManageStoreIcon/>
-        <a href="/dashboard" className="sidebar-link">Manage Store</a>
-        </div>
-        <div className="sidebar-item">
-        <SettingsIcon/>
+        <div className="settings-items menu-items">
+          <div className="settings-item sidebar-item">
+            <SettingsIcon />
             <a href="/dashboard" className="sidebar-link">Settings</a>
-        </div>
-        <div className="sidebar-item">
-        <LogoutIcon/>
-        <a href="/dashboard" className="sidebar-link">Log Out</a>
-        </div>
+          </div>
+          <div className="settings-item sidebar-item">
+            <LogoutIcon />
+            <a href="/dashboard" className="sidebar-link">Log Out</a>
+          </div>
         </div>
       </aside>
   );
